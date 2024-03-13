eb5312f4ef9f3544396a2f1ef143d9170eb79e40
yuvaraj-csk <134052574+yuvaraj-csk@users.noreply.github.com>
2024-03-13 20:21:33 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/lifecyclemethods/MainActivity.java
+++ app/src/main/java/com/example/lifecyclemethods/MainActivity.java
@@ -1,6 +1,13 @@
-package com.example.lifecyclemethods;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Toast;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+
 
 import android.os.Bundle;
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "Oncreate called", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Toast toast = Toast.makeText(getApplicationContext(), "OnStart called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Toast toast = Toast.makeText(getApplicationContext(), "OnRestart called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast toast = Toast.makeText(getApplicationContext(), "OnStop called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast toast = Toast.makeText(getApplicationContext(), "OnPause called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast = Toast.makeText(getApplicationContext(), "OnDestroy called", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
